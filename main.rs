f360ac20c6e1f679869db60b409b6d6919bcccf3
arttu <ttu91@hotmail.com>
2023-12-09 16:15:41 +0200

    Day 1, part 2 done

--- main.rs
+++ main.rs
@@ -28,9 +28,11 @@ static example_data: &str = "467..114..
  3. tulosta
 */
 
+#[derive(Copy, Clone)]
 struct Loc {
     x: i32,
     y: i32,
+    c: char,
 }
 
 impl Loc {
@@ -45,6 +47,7 @@ impl fmt::Display for Loc {
     }
 }
 
+#[derive(Clone)]
 struct Num {
     locs: Vec<Loc>,
     value: i32,
@@ -64,7 +67,11 @@ fn add_number(build: &str, things: &mut Vec<Num>, x: i32, y: i32) {
     let value = build.to_string().parse::<i32>().unwrap();
     let mut locs: Vec<Loc> = Vec::new();
     for loc_x in x - (build.len() as i32)..x {
-        locs.push(Loc { x: loc_x as i32, y });
+        locs.push(Loc {
+            x: loc_x as i32,
+            y,
+            c: '?',
+        });
     }
     let num = Num { locs, value };
     things.push(num);
@@ -88,7 +95,7 @@ fn parse_row(row: &str, y: i32) -> (Vec<Num>, Vec<Loc>) {
                 build = "".to_owned();
             }
             if c != '.' {
-                let symbol = Loc { x: x as i32, y };
+                let symbol = Loc { x: x as i32, y, c };
                 symbols.push(symbol);
             }
         }
@@ -128,11 +135,41 @@ fn get_sum(numbers: &Vec<Num>, symbols: &Vec<Loc>) -> i32 {
     sum
 }
 
-fn compute(data: &str) {
+fn gear_ratio_sum(numbers: &Vec<Num>, symbols: &Vec<Loc>) -> i32 {
+    let mut sum = 0;
+    for symbol in symbols {
+        if (symbol.c != '*') {
+            continue;
+        }
+        let mut gear_numbers: Vec<Num> = Vec::new();
+        for number in numbers.clone() {
+          let mut touches = false;
+          for loc in &number.locs {
+            if (loc.touches(symbol)) {
+              touches = true;
+            }
+          }
+          if (touches) { gear_numbers.push(number) }
+        }
+        if (gear_numbers.len() == 2) {
+          sum += gear_numbers.get(0).unwrap().value * gear_numbers.get(1).unwrap().value;
+        }
+    }
+  sum
+}
+
+fn compute_part_1(data: &str) {
     let rows = data.split("\n").collect::<Vec<&str>>();
     let (numbers, symbols) = parse_rows(rows);
     let answer = get_sum(&numbers, &symbols);
     println!("{}", answer);
+}
+
+fn compute_part_2(data: &str) {
+    let rows = data.split("\n").collect::<Vec<&str>>();
+    let (numbers, symbols) = parse_rows(rows);
+    let answer = gear_ratio_sum(&numbers, &symbols);
+    println!("{}", answer)
 }
 
 fn main() {
@@ -141,8 +178,14 @@ fn main() {
         let file_path = &args[1];
         println!("Reading from file '{}' ...", file_path);
         let data = fs::read_to_string(file_path).expect("Could not read file!\n");
-        compute(&data);
+        println!("Part 1:");
+        compute_part_1(&data);
+        println!("Part 2:");
+        compute_part_2(&data);
     } else {
-      compute(example_data);
+        println!("Part 1:");
+        compute_part_1(example_data);
+        println!("Part 2:");
+        compute_part_2(example_data);
     }
 }
